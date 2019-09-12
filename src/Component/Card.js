de80410d039396e1d3cb3d164917eb2f2303c972
vannasok92 <vannasok92@yahoo.com>
2019-09-12 15:43:01 -0700

    styling

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -12,19 +12,26 @@ color:
 const TextStyle = styled.p`
 color: #483D8B;
 font-weight: 800;
-font-size: 2rem;
+font-size: 1.5rem;
+
+
 `;
 const ImgStyle = styled.img`
 border: 1px solid white;
 border-radius: 10px;
 box-shadow: 1px 5px 12px 1px rgba(255,255,255, 0.2);
 `;
+const TitleStyle = styled.h2`
+background: white;
+border-top: 2px solid #D3D3D3;
+font-size: 2rem;
+`;
 
 const Card = props => {
 
     return(
         <CardStyle>
-            <h2>{props.title}</h2>
+            <TitleStyle>{props.title}</TitleStyle>
             <TextStyle>Description: {props.explanation}</TextStyle>
             <ImgStyle className='img' alt='' src={props.hdurl} />
             <p>Date: {props.date}</p>
